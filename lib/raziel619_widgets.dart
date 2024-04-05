b7c11d46e6d290e443ccda7f0ae31767b4ee6755
Raziel619 <raziel619dev@gmail.com>
2024-04-05 10:07:56 -0400

    Add base files

---
+++ lib/raziel619_widgets.dart
@@ -0,0 +1,1 @@
+export 'widgets/raziel619_store_button.dart';
---
+++ lib/widgets/raziel619_store_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Raziel619StoreButton extends StatelessWidget {
+  const Raziel619StoreButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          print("hello");
+        },
+        child: Text("Raziel619"));
+  }
+}
